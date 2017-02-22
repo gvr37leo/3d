3a2311ff0bd585b6b115e1ef9578b81b970ab4c8
Paul Boon <gvr37.p.l.boon@gmail.com>
2017-02-22 23:31:16 +0100

    removed some repetition

--- src/Vector.java
+++ src/Vector.java
@@ -1,5 +1,6 @@
 public abstract class Vector <T extends Vector<T>>{
     int dimensions;
+    private static float sum;
 
     public static Vector construct(int dimensions){
         switch(dimensions){
@@ -9,38 +10,39 @@ public abstract class Vector <T extends Vector<T>>{
     }
 
     public T add(T v){
-        iterate((i) -> set(i, get(i) + v.get(i)));
-        return (T)this;
+        return iterate((i) -> set(i, get(i) + v.get(i)));
     }
 
     public T sub(T v){
-        iterate((i) -> set(i, get(i) - v.get(i)));
-        return (T)this;
+        return iterate((i) -> set(i, get(i) - v.get(i)));
     }
 
     public T scale(float s){
-        iterate((i) -> set(i, get(i) * s));
-        return (T)this;
+        return iterate((i) -> set(i, get(i) * s));
     }
 
     public T lerp(T v, float weight){
-        return c().add(v.c().sub((T)this).scale(weight));
+        return c().add(v.c().sub(This()).scale(weight));
     }
 
     public float dot(T v){
-        float sum = 0;
-        for(int i = 0; i < dimensions; i++)sum += get(i) + v.get(i);
+        sum = 0;
+        iterate((i) -> sum += get(i) + v.get(i));
         return sum;
     }
 
     public T project(T v){
-        return this.c().scale(this.dot(v) / this.dot((T)this));
+        return this.c().scale(this.dot(v) / this.dot(This()));
     }
 
     public float length(){
-        float sum = 0;
-        for(int i = 0; i < dimensions; i++)sum += get(i) * get(i);
-        return (float) Math.sqrt(sum);
+        return (float) Math.sqrt(lengthSq());
+    }
+
+    public float lengthSq(){
+        sum = 0;
+        iterate((i) -> sum += get(i) * get(i));
+        return sum;
     }
 
     public T normalize(){
@@ -49,16 +51,16 @@ public abstract class Vector <T extends Vector<T>>{
 
     public T c(){
         T c = (T)Vector.construct(dimensions);
-        return (T)iterate((i) -> c.set(i, get(i)));
+        return iterate((i) -> c.set(i, get(i)));
     }
 
-    public Vector overwrite(Vector v){
+    public T overwrite(Vector v){
         return iterate((i) -> set(i, v.get(i)));
     }
 
-    public Vector iterate(Iterator iterator){
+    public T iterate(Iterator iterator){
         for(int i = 0; i < dimensions; i++)iterator.iterate(i);
-        return this;
+        return This();
     }
 
     interface Iterator{
@@ -66,8 +68,8 @@ public abstract class Vector <T extends Vector<T>>{
     }
 
     public abstract float get(int i);
-
     public abstract void set(int i, float val);
+    public abstract T This();
 
     public boolean equals(T v){
         for(int i = 0; i < 3; i++)if(get(i) != v.get(i)) return false;
@@ -75,51 +77,6 @@ public abstract class Vector <T extends Vector<T>>{
     }
 }
 
-class Vector3 extends Vector<Vector3>{
-    float x;
-    float y;
-    float z;
-
-    Vector3(float x, float y, float z){
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        this.dimensions = 3;
-    }
-
-    Vector3(){
-        this.x = 0;
-        this.y = 0;
-        this.z = 0;
-        this.dimensions = 3;
-    }
-
-    public float get(int i){
-        switch (i){
-            case 1:return y;
-            case 2:return z;
-            default: return x;
-        }
-    }
-
-    public void set(int i, float val){
-        switch (i){
-            case 1:y = val;
-                break;
-            case 2:z = val;
-                break;
-            default: x = val;
-        }
-    }
-
-    public Vector3 cross(Vector3 v){
-        return new Vector3(
-                y * v.z - z * v.y,
-                z * v.x - x * v.z,
-                x * v.y - y * v.x
-        );
-    }
-}
 
 class Vector2 extends Vector<Vector2>{
     float x;
@@ -144,5 +101,41 @@ class Vector2 extends Vector<Vector2>{
                 break;
             default: x = val;
         }
+    }
+
+    public Vector2 This() {
+        return this;
+    }
+}
+
+
+class Vector3 extends Vector2{
+    float z;
+
+    Vector3(float x, float y, float z){
+        super(x,y);
+        this.z = z;
+        this.dimensions = 3;
+    }
+
+    public float get(int i){
+        if(i == 2)return z;
+        return super.get(i);
+    }
+
+    public void set(int i, float val){
+        if(i == 2){
+            z = val;
+            return;
+        }
+        super.set(i, val);
+    }
+
+    public Vector3 cross(Vector3 v){
+        return new Vector3(
+                y * v.z - z * v.y,
+                z * v.x - x * v.z,
+                x * v.y - y * v.x
+        );
     }
 }
